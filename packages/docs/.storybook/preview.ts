d5d1f0440f1e5946205fbd32445058528300b3ed
Wesed <dev.wfeduardo@gmail.com>
2023-10-04 18:45:36 -0300

    DS - documentacao das cores

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
@@ -9,10 +10,13 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    docs: {
+      theme: themes.dark,
+  },
     backgrounds: {
       default: 'dark',
     },
-  },
+  }  
 };
 
 export default preview;
--- packages/docs/storybook-static/assets/preview-6a809165.js
+++
@@ -1,2 +0,0 @@
-const a={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},backgrounds:{default:"dark"}}};export{a as default};
-//# sourceMappingURL=preview-6a809165.js.map
